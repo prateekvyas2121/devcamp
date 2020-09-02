a5511bd1cd854d1cf8d1875a0aaa0b0a932752fc
prateekvyas2121 <prateek.15jiec074@jietjodhpur.ac.in>
2020-09-02 09:26:57 +0530

    initial commit test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # comment for testing 
+  #this is new line
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'from@laziestprogrammer.com'
   layout 'mailer'
 end
